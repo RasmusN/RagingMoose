b03d7a6ff663fad8ece4ffc65c13f58d700f4d66
nords <nordstrom.rasmus@gmail.com>
2021-05-08 01:12:59 +0200

    Truck can now turn left and right using A and D

--- Assets/Scripts/TruckController.cs
+++ Assets/Scripts/TruckController.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class TruckController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float SteeringVelocity;
 
     // Update is called once per frame
     void Update()
     {
-        
+       if(Input.GetKey(KeyCode.A)) {
+           gameObject.transform.position += new Vector3(-SteeringVelocity, 0f, 0f) * Time.deltaTime;
+       }
+       if(Input.GetKey(KeyCode.D)) {
+           gameObject.transform.position += new Vector3(SteeringVelocity, 0f, 0f) * Time.deltaTime;
+       }
     }
 }
