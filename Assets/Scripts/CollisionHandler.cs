ca21754a497eaa73aabc665d5060d9cab9bd74a5
nords <nordstrom.rasmus@gmail.com>
2021-05-08 12:42:11 +0200

    No losses HP when hit stone, gains score on hit pedestrain

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour{
+    public int PedestrianHitScore = 150;
+    public int HpLostOnHitStone = 1;
+    public GameObject PlayerInfo;
+    private PlayerInfo playerInfo;
+    void Start(){
+        playerInfo = PlayerInfo.GetComponent<PlayerInfo>();
+    }
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.tag.Equals("Standing Pedestrian")){
             Debug.Log("Hit a Standing Pedestrian");
+            playerInfo.Score += PedestrianHitScore; 
+
         } else if(other.tag.Equals("Stone")){
             Debug.Log("Hit a Stone");
+            playerInfo.Hp -= HpLostOnHitStone;
         }
         else{
             Debug.Log($"Hit an unknown object TAG #{other.tag}");
