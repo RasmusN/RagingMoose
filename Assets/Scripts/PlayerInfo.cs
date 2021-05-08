16216f6ba391d7a11a225fbe4dff3c26fb7e46a7
nords <nordstrom.rasmus@gmail.com>
2021-05-09 00:36:44 +0200

    Distance left never goes below 0

--- Assets/Scripts/PlayerInfo.cs
+++ Assets/Scripts/PlayerInfo.cs
@@ -7,7 +7,14 @@ public class PlayerInfo : MonoBehaviour
 {
     public int Hp;
     public int Score;
-    public float DistanceLeft;
+    public float _distanceLeft;
+    public float DistanceLeft {get => _distanceLeft; set{
+        if(value < 0){
+            _distanceLeft = 0;
+        } else {
+            _distanceLeft = value;
+        }
+    }}
     public float Speed;
     public float DefaultRoadSpeed;
     public float DefaultOffRoadSpeed;
