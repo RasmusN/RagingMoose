c5506a4a679b15cb29220e885d869ff1136074a3
Emma Eliasson <gurgelsnurp@hotmail.se>
2021-05-08 12:09:09 +0200

    Added PlayerInfo object in gameScene with its data

---
+++ Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// use FindObjectOfType<PlayerInfo>() in Start() of every object that needs this
+public class PlayerInfo : MonoBehaviour
+{
+    int Hp;
+    int Score;
+    float Speed;
+}
