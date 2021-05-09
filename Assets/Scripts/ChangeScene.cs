11dde22323bf6a133183dbaa06d59277ae973f4e
Emma Eliasson <gurgelsnurp@hotmail.se>
2021-05-09 10:56:27 +0200

    Added moose sounds when hitting obstacle

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    PlayerInfo playerInfo;
     public void menuScene()
     {
         SceneManager.LoadScene(0);
@@ -16,5 +17,11 @@ public class ChangeScene : MonoBehaviour
     public void scoreScene()
     {
         SceneManager.LoadScene(2);
+    }
+    public void destroyPlayerInfo()
+    {
+        playerInfo = PlayerInfo.FindObjectOfType<PlayerInfo>();
+        if (playerInfo == null) return;
+        Destroy(playerInfo.transform.gameObject);
     }
 }
